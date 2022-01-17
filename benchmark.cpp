6849a44b407b3bad6d5af64724179455be0b9fe3
David Siroky <david@siroky.cz>
2022-01-17 10:49:57 +0100

    benchmark list of small structs

--- benchmark.cpp
+++ benchmark.cpp
@@ -14,14 +14,15 @@ int main(int argc, char* argv[])
     return 0;
 }
 
-namespace {
+struct SmallStruct {};
 
-void BM_jsonwriter_simple_small_struct(benchmark::State& state)
-{
-    auto out = fmt::memory_buffer();
-
-    for (auto _ : state) {
-        jsonwriter::write(out, [](auto& object) {
+namespace jsonwriter {
+template<>
+struct Formatter<SmallStruct> {
+    template<typename Output>
+    static void write(Output& output, const SmallStruct)
+    {
+        jsonwriter::write(output, [](auto& object) {
             object["k1"] = "cd";
             object["k2"] = [](auto& nested_object) {
                 nested_object["o1"] = {1, 2, 999999999};
@@ -32,12 +33,38 @@ void BM_jsonwriter_simple_small_struct(benchmark::State& state)
             object["k3"] = false;
             object["k4"] = 234.345678;
         });
+    }
+};
+} //
+
+namespace {
+
+void BM_jsonwriter_simple_small_struct(benchmark::State& state)
+{
+    auto out = fmt::memory_buffer();
+
+    for (auto _ : state) {
+        jsonwriter::write(out, SmallStruct{});
         benchmark::DoNotOptimize(out.data());
         benchmark::ClobberMemory();
         out.clear();
     }
 }
 BENCHMARK(BM_jsonwriter_simple_small_struct);
+
+void BM_jsonwriter_simple_small_struct_list(benchmark::State& state)
+{
+    auto out = fmt::memory_buffer();
+    std::vector<SmallStruct> vec{1000};
+
+    for (auto _ : state) {
+        jsonwriter::write(out, vec);
+        benchmark::DoNotOptimize(out.data());
+        benchmark::ClobberMemory();
+        out.clear();
+    }
+}
+BENCHMARK(BM_jsonwriter_simple_small_struct_list);
 
 void BM_jsonwriter_large_strings(benchmark::State& state)
 {
