03b5d75169658edeb7a8cc2a525fbac01311174c
David Siroky <david@siroky.cz>
2022-02-16 18:06:27 +0100

    cleanup

--- jsonwriter/int.hpp
+++ jsonwriter/int.hpp
@@ -10,21 +10,6 @@ namespace jsonwriter {
 
 class FormatInt
 {
-    static constexpr char digit_pairs[201] = {"00010203040506070809"
-                                              "10111213141516171819"
-                                              "20212223242526272829"
-                                              "30313233343536373839"
-                                              "40414243444546474849"
-                                              "50515253545556575859"
-                                              "60616263646566676869"
-                                              "70717273747576777879"
-                                              "80818283848586878889"
-                                              "90919293949596979899"};
-
-    // fit int64_t
-    static const int BUFFER_SIZE = 20;
-    char buf[BUFFER_SIZE];
-
 public:
     template<typename T>
     std::string_view itostr(const T val)
@@ -39,6 +24,21 @@ public:
 
 private:
     // taken from https://stackoverflow.com/questions/4351371/c-performance-challenge-integer-to-stdstring-conversion
+
+    static constexpr char digit_pairs[201] = {"00010203040506070809"
+                                              "10111213141516171819"
+                                              "20212223242526272829"
+                                              "30313233343536373839"
+                                              "40414243444546474849"
+                                              "50515253545556575859"
+                                              "60616263646566676869"
+                                              "70717273747576777879"
+                                              "80818283848586878889"
+                                              "90919293949596979899"};
+
+    // fit int64_t
+    static constexpr int BUFFER_SIZE{20};
+    char buf[BUFFER_SIZE];
 
     std::string_view itostr_int(int64_t val)
     {
